077b2ab40c74f9750d5e7f0330dcd19654d0213a
Paul Hoekman <paul@penguinprotocols.com>
2021-04-27 17:03:46 +0200

    Prep for shapefiles 30

--- core/admin.py
+++ core/admin.py
@@ -5,3 +5,11 @@ class SearchAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 admin.site.register(Photo, SearchAdmin)
+admin.site.register(Page, SearchAdmin)
+admin.site.register(Garden, SearchAdmin)
+admin.site.register(Shapefile, SearchAdmin)
+admin.site.register(Corridor, SearchAdmin)
+admin.site.register(Event, SearchAdmin)
+admin.site.register(Genus, SearchAdmin)
+admin.site.register(Species, SearchAdmin)
+admin.site.register(Redlist, SearchAdmin)
