779c5d70982e83babb40fc9e7811080fef8cc683
Paul Hoekman <paul@penguinprotocols.com>
2021-10-06 16:54:43 -0500

    Background and header changes 21

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+import random
 
 def site(request):
 
@@ -8,7 +9,11 @@ def site(request):
             work_offline = True
     except:
         work_offline = False
+
+    header_bg = random.choice(range(0,44))
+
     return {
         "DEBUG": settings.DEBUG,
         "WORK_OFFLINE": True if work_offline else False,
+        "HEADER_BG": f"/img/bg/{header_bg}.jpg",
     }
